850a1d3a9dd314eb849568831cd399d6816b37e7
patrick-tr <paatz1996@gmail.com>
2020-06-01 19:36:12 +0200

    Added CheckBox binding and LogSaveBtnEvent

--- AnwesenheitsApp/AnwesenheitsApp/ExportPage.xaml.cs
+++ AnwesenheitsApp/AnwesenheitsApp/ExportPage.xaml.cs
@@ -12,9 +12,47 @@ namespace AnwesenheitsApp
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class ExportPage : ContentPage
     {
+        private bool _isCSV;
+        private bool _isEXCEL;
+
+        public bool isCSV
+        {
+            get
+            {
+                return this._isCSV;
+            }
+            set
+            {
+                this._isCSV = value;
+                OnPropertyChanged();
+            }
+        }
+        public bool isEXCEL
+        {
+            get
+            {
+                return this._isEXCEL;
+            }
+            set
+            {
+                this._isEXCEL = value;
+                OnPropertyChanged();
+            }
+        }
+
         public ExportPage()
         {
+            this.isCSV = false;
+            this.isEXCEL = false;
+
             InitializeComponent();
+
+            this.BindingContext = this;
+        }
+
+        public void SaveLogBtnClicked(object sender, EventArgs e)
+        {
+            
         }
     }
 }
